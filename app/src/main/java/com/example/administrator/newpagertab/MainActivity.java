f664e3b7226455270f623753e393c6ed21e7dbb6
Administrator <1101144920@qq.com>
2016-05-25 17:10:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/newpagertab/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.administrator.newpagertab;
+
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.administrator.newpagertab.view.PagerSlidingTabStrip;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PagerSlidingTabStrip tvTitle;
+    private ViewPager vpTest;
+    private ArrayList<String> mList;
+
+
+    private void assignViews() {
+        tvTitle = (PagerSlidingTabStrip) findViewById(R.id.location_tabs);
+        vpTest = (ViewPager) findViewById(R.id.location_pager);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        assignViews();
+        addData();
+        vpTest.setAdapter(new MyAdapter());
+        tvTitle.setViewPager(vpTest);
+    }
+
+    private void addData() {
+        mList=new ArrayList<>();
+        for(int i=0;i<2;i++) {
+            mList.add("" + i);
+        }
+    }
+
+    class MyAdapter extends PagerAdapter {
+
+        // 指定指示器的标题
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String data = mList.get(position);
+            return data;
+        }
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            String pager = mList.get(position);
+
+            View view = View.inflate(MainActivity.this,R.layout.viewpage_item, null);
+            TextView tv= (TextView) view.findViewById(R.id.textview);
+            tv.setText(pager);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+    }
+
+
+}
